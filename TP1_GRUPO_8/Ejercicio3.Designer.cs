beb64a0459769101d3f561e967f35f825498932f
IvanMoreiraUTN <ivan.moreira2@alumnos.frgp.utn.edu.ar>
2024-08-13 18:39:49 -0300

    Parte Gráfica Ejercicio 3

--- TP1_GRUPO_8/Ejercicio3.Designer.cs
+++ TP1_GRUPO_8/Ejercicio3.Designer.cs
@@ -28,20 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.clbOficio = new System.Windows.Forms.CheckedListBox();
+            this.btnSeleccion = new System.Windows.Forms.Button();
+            this.gbSexo = new System.Windows.Forms.GroupBox();
+            this.rbFemenino = new System.Windows.Forms.RadioButton();
+            this.rbMasculino = new System.Windows.Forms.RadioButton();
+            this.gbEstadoCivil = new System.Windows.Forms.GroupBox();
+            this.rbSoltero = new System.Windows.Forms.RadioButton();
+            this.rbCasado = new System.Windows.Forms.RadioButton();
+            this.gbSexo.SuspendLayout();
+            this.gbEstadoCivil.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // clbOficio
+            // 
+            this.clbOficio.CheckOnClick = true;
+            this.clbOficio.FormattingEnabled = true;
+            this.clbOficio.Items.AddRange(new object[] {
+            "Data Entry",
+            "Operador de PC",
+            "Programador",
+            "Reparador de PC",
+            "Tester"});
+            this.clbOficio.Location = new System.Drawing.Point(224, 152);
+            this.clbOficio.Name = "clbOficio";
+            this.clbOficio.Size = new System.Drawing.Size(188, 94);
+            this.clbOficio.TabIndex = 0;
+            this.clbOficio.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // btnSeleccion
+            // 
+            this.btnSeleccion.Location = new System.Drawing.Point(224, 263);
+            this.btnSeleccion.Name = "btnSeleccion";
+            this.btnSeleccion.Size = new System.Drawing.Size(188, 23);
+            this.btnSeleccion.TabIndex = 4;
+            this.btnSeleccion.Text = "Mostrar lo que se seleccionó";
+            this.btnSeleccion.UseVisualStyleBackColor = true;
+            // 
+            // gbSexo
+            // 
+            this.gbSexo.Controls.Add(this.rbMasculino);
+            this.gbSexo.Controls.Add(this.rbFemenino);
+            this.gbSexo.Location = new System.Drawing.Point(21, 35);
+            this.gbSexo.Name = "gbSexo";
+            this.gbSexo.Size = new System.Drawing.Size(200, 100);
+            this.gbSexo.TabIndex = 8;
+            this.gbSexo.TabStop = false;
+            this.gbSexo.Text = "Sexo";
+            // 
+            // rbFemenino
+            // 
+            this.rbFemenino.AutoSize = true;
+            this.rbFemenino.Checked = true;
+            this.rbFemenino.Location = new System.Drawing.Point(7, 36);
+            this.rbFemenino.Name = "rbFemenino";
+            this.rbFemenino.Size = new System.Drawing.Size(71, 17);
+            this.rbFemenino.TabIndex = 0;
+            this.rbFemenino.TabStop = true;
+            this.rbFemenino.Text = "Femenino";
+            this.rbFemenino.UseVisualStyleBackColor = true;
+            // 
+            // rbMasculino
+            // 
+            this.rbMasculino.AutoSize = true;
+            this.rbMasculino.Location = new System.Drawing.Point(7, 59);
+            this.rbMasculino.Name = "rbMasculino";
+            this.rbMasculino.Size = new System.Drawing.Size(73, 17);
+            this.rbMasculino.TabIndex = 1;
+            this.rbMasculino.Text = "Masculino";
+            this.rbMasculino.UseVisualStyleBackColor = true;
+            // 
+            // gbEstadoCivil
+            // 
+            this.gbEstadoCivil.Controls.Add(this.rbSoltero);
+            this.gbEstadoCivil.Controls.Add(this.rbCasado);
+            this.gbEstadoCivil.Location = new System.Drawing.Point(418, 35);
+            this.gbEstadoCivil.Name = "gbEstadoCivil";
+            this.gbEstadoCivil.Size = new System.Drawing.Size(200, 100);
+            this.gbEstadoCivil.TabIndex = 9;
+            this.gbEstadoCivil.TabStop = false;
+            this.gbEstadoCivil.Text = "Estado Civil";
+            // 
+            // rbSoltero
+            // 
+            this.rbSoltero.AutoSize = true;
+            this.rbSoltero.Location = new System.Drawing.Point(7, 59);
+            this.rbSoltero.Name = "rbSoltero";
+            this.rbSoltero.Size = new System.Drawing.Size(58, 17);
+            this.rbSoltero.TabIndex = 1;
+            this.rbSoltero.Text = "Soltero";
+            this.rbSoltero.UseVisualStyleBackColor = true;
+            // 
+            // rbCasado
+            // 
+            this.rbCasado.AutoSize = true;
+            this.rbCasado.Checked = true;
+            this.rbCasado.Location = new System.Drawing.Point(7, 36);
+            this.rbCasado.Name = "rbCasado";
+            this.rbCasado.Size = new System.Drawing.Size(61, 17);
+            this.rbCasado.TabIndex = 0;
+            this.rbCasado.TabStop = true;
+            this.rbCasado.Text = "Casado";
+            this.rbCasado.UseVisualStyleBackColor = true;
             // 
             // Ejercicio3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(676, 483);
+            this.Controls.Add(this.gbEstadoCivil);
+            this.Controls.Add(this.gbSexo);
+            this.Controls.Add(this.btnSeleccion);
+            this.Controls.Add(this.clbOficio);
             this.Name = "Ejercicio3";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ejercicio3";
+            this.gbSexo.ResumeLayout(false);
+            this.gbSexo.PerformLayout();
+            this.gbEstadoCivil.ResumeLayout(false);
+            this.gbEstadoCivil.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox clbOficio;
+        private System.Windows.Forms.Button btnSeleccion;
+        private System.Windows.Forms.GroupBox gbSexo;
+        private System.Windows.Forms.RadioButton rbMasculino;
+        private System.Windows.Forms.RadioButton rbFemenino;
+        private System.Windows.Forms.GroupBox gbEstadoCivil;
+        private System.Windows.Forms.RadioButton rbSoltero;
+        private System.Windows.Forms.RadioButton rbCasado;
     }
 }
--- TP1_GRUPO_8/Ejercicio3.cs
+++ TP1_GRUPO_8/Ejercicio3.cs
@@ -16,5 +16,30 @@ namespace TP1_GRUPO_8
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSexo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
